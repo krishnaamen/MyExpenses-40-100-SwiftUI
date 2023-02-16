d34d90dc7aaa14f280c052734b044e9e789482b6
Krishna Prasad Khanal <krishnaamen@gmail.com>
2023-02-16 09:28:11 +0100

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by min phone on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
